d0f10e9bc9c55a692c350c806de48498f18126ea
Arne Dußin <arne.dussin@live.de>
2018-05-31 18:39:59 +0200

    Rename/fix inkscape files. Build round version

--- make.py
+++ make.py
@@ -8,6 +8,8 @@ from subprocess import call
 
 DEBUG = True
 
+SRC_FOLDER = 'src/'
+
 DEFAULT_SIZE = 512
 DEFAULT_COLOR = 'green'
 DEFAULT_DIR = 'output'
@@ -37,12 +39,12 @@ def mkdir(path: str):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def export(colors: [str] = [DEFAULT_COLOR], size: int = DEFAULT_SIZE, svg_dir: str = DEFAULT_DIR, png: bool = True, png_dir: str = DEFAULT_DIR):
+def export(base_file: str, colors: [str] = [DEFAULT_COLOR], size: int = DEFAULT_SIZE, svg_dir: str = DEFAULT_DIR, png: bool = True, png_dir: str = DEFAULT_DIR):
     mkdir(svg_dir)
     mkdir(png_dir)
 
     # Open source file
-    tree = ET.parse('src/base.ink.svg')
+    tree = ET.parse(SRC_FOLDER + base_file + '.ink.svg')
     root = tree.getroot()
 
     for c in colors:
@@ -60,15 +62,15 @@ def export(colors: [str] = [DEFAULT_COLOR], size: int = DEFAULT_SIZE, svg_dir: s
         t.set('fill','url(#' + c_id + ')' )
         t.set('stroke','url(#' + c_id + ')' )
 
-        tree.write(svg_dir + '/logo_' + c + '.svg')
+        tree.write(svg_dir + '/' + base_file + '_' + c + '.svg')
 
         # Generate the .png-files, if it has been selected to do so
         if png:
             res = call(['inkscape',
                   '--export-png',
-                  png_dir + '/logo' + str(size) + 'px_' + c + '.png',
+                  png_dir + '/' + base_file + '_' + str(size) + 'px_' + c + '.png',
                   '--export-height=' + str(size),
-                  svg_dir + '/logo_' + c + '.svg'])
+                  svg_dir + '/' + base_file + '_' + c + '.svg'])
 
             if res != 0:
                 # The build failed. Abort the process
@@ -81,12 +83,14 @@ def main():
     # Commandline options
     parser = argparse.ArgumentParser(description = 'Generate FOSS-AG design bitmaps from SVG source files')
 
-    parser.add_argument('-a', '--all', action = 'store_true', help = 'Build all colors available')
+    parser.add_argument('-a', '--all', action = 'store_true', help = 'Build all colors and all flavours available')
     parser.add_argument('--clean', action = 'store_true', help = 'Remove all generated files. All other options will be ignored')
     parser.add_argument('-p', '--png_only', action = 'store_true', help = 'Only store the png files. The SVG files will still need to be generated, but will be stored in the tmp dir')
     parser.add_argument('-v', '--svg_only', action = 'store_true', help = 'Only generate the SVG files')
     parser.add_argument('-s', '--size', nargs = '?', type = int, default = DEFAULT_SIZE, help = 'The size the generated bitmap file(s) should have. Default is ' + str(DEFAULT_SIZE))
     parser.add_argument('-c', '--colors', nargs = '*', default = [DEFAULT_COLOR], help = 'The colors the script should generate. Default is single ' + DEFAULT_COLOR + ' version. This argument will be ignored when the --all flag is set')
+    parser.add_argument('-r', '--round', action = 'store_true', help = 'Build the round version of the logo. Overwritten by all')
+    parser.add_argument('-q', '--quadratic', action = 'store_true', help = 'Build the quadratic version. This is default and only needed when building multiple versions.')
 
     # Read command line input
     args = parser.parse_args()
@@ -106,9 +110,24 @@ def main():
         print('--png_only and --svg_only cannot be set at the same time. Aborting')
         return 1
 
-    # Make sure all colors are built when the all option is set
+    # The files that are used to build from. By default it is the quadratic base
+    # file
+    base_files = []
+    if not args.all:
+        if args.round:
+            base_files.append('logo_round')
+        if args.quadratic:
+            base_files.append('logo_square')
+
+        # If none are set, the default base.ink.svg will be built
+        if not args.round and not args.quadratic:
+            base_files.append('logo_square')
+
+    # Make sure all colors and flavours are built when the all option is set
     if args.all:
         args.colors = ACCENT_PALETTE
+        base_files.append('logo_square')
+        base_files.append('logo_round')
 
     svg_dir = DEFAULT_DIR;
     if args.png_only:
@@ -116,7 +135,10 @@ def main():
 
     print('Building the following colors:', args.colors)
 
-    return export(args.colors, args.size, svg_dir, not args.svg_only, DEFAULT_DIR)
+    successful = True
+    for flavour in base_files:
+        print('Making file: ', flavour)
+        export(flavour, args.colors, args.size, svg_dir, not args.svg_only, DEFAULT_DIR)
 
 if __name__ == "__main__":
     main()
